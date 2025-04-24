7c74fededc36c9406b4710bae9ba318aa1d4ae7b
rikka <admin@rikka.cc>
2025-04-24 10:08:17 +0800

    chore(deps): update deck.gl and luma.gl dependencies to v9.1.x
    - Update @deck.gl/* packages from 9.0.41 to 9.1.11
    - Update @luma.gl/core and @luma.gl/webgl from 9.0.28 to 9.1.9
    - Remove ignore rules for /deck.gl/ and /luma.gl/ in taze.config.ts
    - Clear matchPackageNames for /deck.gl/ and /luma.gl/ in renovate.json

--- taze.config.ts
+++ taze.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "taze";
 
 export default defineConfig({
   packageMode: {
-    "/deck.gl/": "ignore",
-    "/luma.gl/": "ignore",
+    // "/deck.gl/": "ignore",
+    // "/luma.gl/": "ignore",
   },
 });
